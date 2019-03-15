dafe4875cfc1a13360eae90f2d11ac3bf324820e
prabhjyot28 <tryyourrajat@gmail.com>
2019-03-15 23:56:44 +0530

    Updates

---
+++ precache-manifest.19f47e7469ab7f861ecb24ec77a82dec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcb73106ddb1f86d5cc5",
+    "url": "/Bank-Search/static/js/runtime~main.46046f0e.js"
+  },
+  {
+    "revision": "9b94687e391385ddc265",
+    "url": "/Bank-Search/static/js/main.01001a70.chunk.js"
+  },
+  {
+    "revision": "690c34041b4145f66571",
+    "url": "/Bank-Search/static/js/2.28050d7c.chunk.js"
+  },
+  {
+    "revision": "9b94687e391385ddc265",
+    "url": "/Bank-Search/static/css/main.5366e252.chunk.css"
+  },
+  {
+    "revision": "690c34041b4145f66571",
+    "url": "/Bank-Search/static/css/2.9965129e.chunk.css"
+  },
+  {
+    "revision": "df9862cb6cde3404a3a27a31a3cc6354",
+    "url": "/Bank-Search/index.html"
+  }
+];
